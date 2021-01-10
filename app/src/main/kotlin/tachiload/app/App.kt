8a310aa9dce04e428e83a651fd802667d14c3463
DrosoCode <internet-site@outlook.com>
2021-01-10 23:10:50 +0100

    Fix error with cli

--- app/src/main/kotlin/tachiload/app/App.kt
+++ app/src/main/kotlin/tachiload/app/App.kt
@@ -7,35 +7,38 @@ import kotlinx.cli.*
 
 fun main(args: Array<String>) {
 
-    val parser = ArgParser("Tachiload")
-    val cron by parser.option(ArgType.String, shortName = "c", description = "Cron Expression")
-    val webhook by parser.option(ArgType.String, shortName = "wh", description = "Webhook URL (discord) for notification")
-    val configPath by parser.option(ArgType.String, shortName = "cfg", description = "Path to configuration file").default("./config.json")
-    val downloadPath by parser.option(ArgType.String, shortName = "dl", description = "Path to download directory").default("./downloads")
-    parser.parse(args)
-
-    if(cron != null)
+    if(args.isNotEmpty() && (args[0] == "--extensions" || args[0] == "--search"))
     {
-        println("========================================== Tachiload [CRON] ==========================================")
-        val s = Scheduler()
-        s.schedule(cron, Runnable() {
-            println("Running update at:"+LocalDateTime.now())
-            Download(configPath, downloadPath, webhook).update()
-        })
-        s.start()
-        try {
-            Thread.sleep(Long.MAX_VALUE)
-        } catch (e: InterruptedException) {
-        }
-    }
-    else if(args.isNotEmpty() && (args[0] == "--extensions" || args[0] == "--search"))
-    {
-        CLI(configPath, args)
+        CLI("./config.json", args)
     }
     else
     {
-        println("========================================== Tachiload ==========================================")
-        Download(configPath, downloadPath, webhook).update()
+        val parser = ArgParser("Tachiload")
+        val cron by parser.option(ArgType.String, shortName = "c", description = "Cron Expression")
+        val webhook by parser.option(ArgType.String, shortName = "wh", description = "Webhook URL (discord) for notification")
+        val configPath by parser.option(ArgType.String, shortName = "cfg", description = "Path to configuration file").default("./config.json")
+        val downloadPath by parser.option(ArgType.String, shortName = "dl", description = "Path to download directory").default("./downloads")
+        parser.parse(args)
+
+        if(cron != null)
+        {
+            println("========================================== Tachiload [CRON] ==========================================")
+            val s = Scheduler()
+            s.schedule(cron, Runnable() {
+                println("Running update at:"+LocalDateTime.now())
+                Download(configPath, downloadPath, webhook).update()
+            })
+            s.start()
+            try {
+                Thread.sleep(Long.MAX_VALUE)
+            } catch (e: InterruptedException) {
+            }
+        }
+        else
+        {
+            println("========================================== Tachiload ==========================================")
+            Download(configPath, downloadPath, webhook).update()
+        }
     }
     exitProcess(0)
 }
